c1c6eecd6198c25ca94e6911ada63c5869cf3610
Joshua Eckert <76778947+tokirodesu@users.noreply.github.com>
2024-11-05 08:08:57 +0100

    Update: correct session model synrax

--- api/model/session.model.js
+++ api/model/session.model.js
@@ -1,17 +1,22 @@
-// model/session.js
-const mongoose = require('mongoose');
+// models/session.model.js
+module.exports = mongoose => {
+  const schema = mongoose.Schema(
+    {
+      session_code: { type: String, required: true, unique: true },
+      players: [
+        { type: mongoose.Schema.Types.ObjectId, ref: 'Player' }
+      ]
+    },
+    { timestamps: true }
+  );
 
-const sessionSchema = new mongoose.Schema({
-  session_code: {
-    type: String,
-    required: true,
-    unique: true, // Ensure unique session codes
-  },
-  players: {
-    type: [String], // Assuming players are stored as strings (names)
-    required: true,
-  },
-});
+  // Custom method to format the JSON output
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
 
-module.exports = mongoose.model('Session', sessionSchema);
-
+  const Session = mongoose.model("Session", schema);
+  return Session;
+};
